5d1fcccda1194857c0d456a39497d2a67e9e6bcf
Nik Sauer <nik.sauer@luftdaten.at>
2024-11-07 14:30:46 +0100

    #18 added alembic migration to add hourly_avg view

---
+++ code/alembic/versions/8e1c45de1275_create_hourly_avg_view.py
@@ -0,0 +1,32 @@
+"""create hourly_avg view
+
+Revision ID: 8e1c45de1275
+Revises: 5516d2dd3e78
+Create Date: 2024-11-07 14:21:42.941381
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '8e1c45de1275'
+down_revision: Union[str, None] = '5516d2dd3e78'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute("""create view hourly_avg as
+select station_id, dimension, date_trunc('hour', time_measured) as hour, avg(value)
+from measurements as m join values as v on v.measurement_id = m.id
+join stations as s on s.id = m.station_id
+group by station_id, dimension, hour 
+order by station_id, dimension, hour;""")
+
+
+
+def downgrade() -> None:
+    op.execute("drop view if exists hourly_avg")
