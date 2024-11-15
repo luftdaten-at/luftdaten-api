6f140b0627a990c29826bf51f70843faaf9a33c8
Nik Sauer <nik.sauer@luftdaten.at>
2024-11-15 09:58:56 +0100

    save

---
+++ import_scripts/code/database.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import os
+
+# Umgebungsvariablen auslesen
+DB_USER = os.getenv("POSTGRES_USER", "")
+DB_PASS = os.getenv("POSTGRES_PASSWORD", "")
+DB_HOST = os.getenv("DB_HOST", "")
+DB_NAME = os.getenv("POSTGRES_DB", "")
+
+# Erstellen der korrekten DATABASE_URL mit f-String
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+
+# Engine und Session erstellen
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Basis-Model für die Deklaration der Datenbanktabellen
+Base = declarative_base()
+
+# Dependency to get the DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- import_scripts/code/main.py
+++ import_scripts/code/main.py
@@ -1,16 +1,31 @@
-from bs4 import BeautifulSoup
 import requests
-from urllib.parse import urljoin
 import re
 import zipfile
 import io
 import pandas as pd
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+from database import get_db
+from models import *
 
 
 def import_sensor_community_archive_from_csv(csv: str):
+    """
+    sensor_id;sensor_type;location;lat;lon;timestamp;pressure;altitude;pressure_sealevel;temperature
+    """
+    db = next(get_db())
     df = pd.read_csv(io.BytesIO(csv.encode()), encoding='utf8', sep=";")
-    print(df)
 
+    device_set = set(
+        db.query(Station)
+        .filter(Station.source == 3)
+        .all()
+    )
+
+    for row in df.iterrows():
+        # check if sensor_id in database
+        # if yes add all measurements if they are not already in the database
+        print(row[1]['sensor_id'])
 
 url = "https://archive.sensor.community/csv_per_month/"
 
