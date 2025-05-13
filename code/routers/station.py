fdccc463cf51bc53f29ce094d6634a4245cc1b54
Nik Sauer <nik.sauer@luftdaten.at>
2025-05-13 14:56:45 +0200

    changed calibration endpoint

--- code/routers/station.py
+++ code/routers/station.py
@@ -23,21 +23,33 @@ router = APIRouter()
 
 @router.get('/calibration', response_class=Response, tags=['station', 'calibration'])
 async def get_calibration_data(
+    station_ids: str = None,
+    data: bool = True,
     db: Session = Depends(get_db)
 ):
+    stations = db.query(Station).join(Station.calibration_measurements).all()
+    if station_ids is not None:
+        station_id_list = station_ids.split(",")
+        stations = db.query(Station).filter(Station.device.in_(station_id_list)).all()
     # csv
     # device id, sensor.model, dimension, value, time
     csv = []
-    measurements = db.query(CalibrationMeasurement).all()
-    for m in measurements:
-        for v in m.values:
-            csv.append(','.join(str(x) for x in [
-                m.station.device,
-                m.sensor_model,
-                v.dimension,
-                v.value,
-                m.time_measured,
-            ]))
+    if data:
+        measurements = []
+        for station in stations:
+            measurements.extend(db.query(CalibrationMeasurement).filter(CalibrationMeasurement.station.id == station.id).all())
+        for m in measurements:
+            for v in m.values:
+                csv.append(','.join(str(x) for x in [
+                    m.station.device,
+                    m.sensor_model,
+                    v.dimension,
+                    v.value,
+                    m.time_measured,
+                ]))
+    else:
+        for station in stations:
+            csv.append(str(station.device))
     
     return Response(content='\n'.join(csv), media_type="text/csv")
 
