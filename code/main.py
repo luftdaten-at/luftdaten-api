0b97ede52776bed67133e18d744a8b94c47c6445
Silvio Heinze <mail@silvioheinze.eu>
2026-04-10 18:05:58 +0200

    Add localhost to CORS allowlist in `_cors_allowlist()` function
    - Updated the CORS allowlist in `main.py` to include "http://localhost" for improved local development support.

--- code/main.py
+++ code/main.py
@@ -61,6 +61,7 @@ def _cors_allowlist():
             "http://127.0.0.1:3000",
             "http://localhost:5173",
             "http://127.0.0.1:5173",
+            "http://localhost",
         ],
         True,
     )
